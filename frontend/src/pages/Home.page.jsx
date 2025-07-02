210281c3b28ecf0ad4c83d88344b685e5f806c31
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-07-02 19:28:32 +0530

    [feat] (frontend): home page Header section is DONE

--- frontend/src/pages/Home.page.jsx
+++ frontend/src/pages/Home.page.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Header from "../components/Header";
+
 const HomePage = () => {
   return (
     <div>
-      Home
+      <Header />
     </div>
   )
 }
