c34764b3cbcb93e3a71504e9f4a18ef9a459f8be
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-07-02 20:10:04 +0530

    [feat] (frontend): home page SpecialityMenu section is DONE

--- frontend/src/pages/Home.page.jsx
+++ frontend/src/pages/Home.page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../components/Header";
+import SpecialityMenu from '../components/SpecialityMenu';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   )
 }
